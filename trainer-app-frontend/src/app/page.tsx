16666c4f2ffb062676ba88e4cf31d7d4b792c585
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-04 18:15:10 -0500

    added hero section

--- trainer-app-frontend/src/app/page.tsx
+++ trainer-app-frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Header } from "@/homeSections/Header";
+import { Hero } from "@/homeSections/Hero";
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Hero />
+    </>
   );
 }
