c634a1c5dc77b48c380e9bb375c08ddae70b0ffe
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-07 18:06:43 -0500

    added textticker and productshowcase

--- trainer-app-frontend/src/app/page.tsx
+++ trainer-app-frontend/src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@/homeSections/Header";
 import { Hero } from "@/homeSections/Hero";
+import { TextTicker } from "@/homeSections/TextTicker";
+import { ProductShowcase } from "@/homeSections/ProductShowcase";
+
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <TextTicker />
+      <ProductShowcase />
     </>
   );
 }
