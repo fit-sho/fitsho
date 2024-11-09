1ce7505e30b4ed4e1fd4e87452735dbc0b6f5079
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-09 13:43:14 -0500

    Added tube and pyramid image

--- trainer-app-frontend/src/homeSections/ProductShowcase.tsx
+++ trainer-app-frontend/src/homeSections/ProductShowcase.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import productImage from "@/assets/images/product-image.png";
 import pyramidImage from "@/assets/images/pyramid.png";
+import starImage from "@/assets/images/star.png";
+import tubeImage from "@/assets/images/tube.png";
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
         <div className="max-w-[540px] mx-auto">
           <div className="flex justify-center">
             <div className="tag">Boost your Productivty</div>
           </div>
-          <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-t from-[#002121] to-[#001E80] text-transparent bg-clip-text mt-5">
+          <h2
+            className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter 
+          bg-gradient-to-t from-[#002121] to-[#001E80] text-transparent bg-clip-text mt-5"
+          >
             A more effective way to track progress
           </h2>
           <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
@@ -21,6 +26,20 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product Showcase" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="Pyramid Image"
+            className="hidden md:block absolute -right-36 -top-32 "
+            height={262}
+            width={262}
+          />
+          <Image
+            src={tubeImage}
+            alt="Tube Image"
+            className="hidden md:block absolute bottom-24 -left-36"
+            height={248}
+            width={248}
+          />
         </div>
       </div>
     </section>
