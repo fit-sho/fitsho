67e5de8cec574c45552ace0943733331c5a369be
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-18 10:52:48 -0500

    added footer section

--- trainer-app-frontend/src/app/page.tsx
+++ trainer-app-frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import { TextTicker } from "@/homeSections/TextTicker";
 import { ProductShowcase } from "@/homeSections/ProductShowcase";
 import { Pricing } from "@/homeSections/Pricing";
 import { CallToAction } from "@/homeSections/CallToAction";
+import { Footer } from "@/homeSections/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <ProductShowcase />
       <Pricing />
       <CallToAction />
+      <Footer />
     </>
   );
 }
