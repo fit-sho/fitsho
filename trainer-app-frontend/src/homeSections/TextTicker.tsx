913f6d883fc8eeaa2926d4e9b64c99e3b4098e0d
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-18 22:30:49 -0500

    updated the text ticker with animation

--- trainer-app-frontend/src/homeSections/TextTicker.tsx
+++ trainer-app-frontend/src/homeSections/TextTicker.tsx
@@ -1,30 +1,41 @@
-import { Fragment } from "react";
+"use client";
+
+import { motion } from "framer-motion";
 
 export const TextTicker = () => {
-  const words = [
-    "Responsive",
-    "Interactive",
-    "Performant",
-    "Accessible",
-    "Secure",
-    "User Friendly",
-    "Practical",
-  ];
   return (
     <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          {[...new Array(2)].fill(0).map((_, idx) => (
-            <Fragment key={idx}>
-              {words.map((word) => (
-                <div key={word} className="flex flex-none ">
-                  <span className="text-2xl font-extrabold text-gray-600 tracking-wide px-8">
-                    {word}
-                  </span>
-                </div>
-              ))}
-            </Fragment>
-          ))}
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
+            <p className="textTicker-words">Responsive</p>
+            <p className="textTicker-words">Interactive</p>
+            <p className="textTicker-words">Performant</p>
+            <p className="textTicker-words">Accessible</p>
+            <p className="textTicker-words">Secure</p>
+            <p className="textTicker-words">User Friendly</p>
+            <p className="textTicker-words">Practical</p>
+
+            {/* Second set of words for animation */}
+            <p className="textTicker-words">Responsive</p>
+            <p className="textTicker-words">Interactive</p>
+            <p className="textTicker-words">Performant</p>
+            <p className="textTicker-words">Accessible</p>
+            <p className="textTicker-words">Secure</p>
+            <p className="textTicker-words">User Friendly</p>
+            <p className="textTicker-words">Practical</p>
+          </motion.div>
         </div>
       </div>
     </div>
