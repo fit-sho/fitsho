1b9b9f3476165334c125c5dfe3ff456c76bb721d
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-21 01:15:19 -0500

    fixed button presses

--- trainer-app-frontend/src/homeSections/Footer.tsx
+++ trainer-app-frontend/src/homeSections/Footer.tsx
@@ -9,12 +9,14 @@ export const Footer = () => {
     <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
       <div className="container">
         <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur  before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
-          <Image
-            src={logo}
-            alt="Fitsho Logo"
-            height={40}
-            className="relative"
-          />
+          <a href="#">
+            <Image
+              src={logo}
+              alt="Fitsho Logo"
+              height={40}
+              className="relative"
+            />
+          </a>
         </div>
         <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
           <a href="#">About</a>
@@ -25,9 +27,15 @@ export const Footer = () => {
           <a href="#">Careers</a>
         </nav>
         <div className="flex justify-center gap-6 mt-6">
-          <SocialInsta />
-          <SocialLinkedin />
-          <SocialYoutube />
+          <a href="https://www.instagram.com/ilestarash/">
+            <SocialInsta />
+          </a>
+          <a href="https://www.linkedin.com/in/arash-shalchian-230b06268/">
+            <SocialLinkedin />
+          </a>
+          <a href="https://www.youtube.com/@arashshalchian">
+            <SocialYoutube />
+          </a>
         </div>
         <p className="mt-6">&copy; 2024 FitSho, Inc. All rights reserved.</p>
       </div>
