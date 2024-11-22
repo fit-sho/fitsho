4cd06cc6b2da48edf3b4c14c2546257738df6f47
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-21 23:23:18 -0500

    added parallax effect

--- trainer-app-frontend/src/homeSections/CallToAction.tsx
+++ trainer-app-frontend/src/homeSections/CallToAction.tsx
@@ -1,11 +1,25 @@
+"use client";
 import ArrowRight from "@/assets/icons/ArrowRight.svg";
 import StarImage from "@/assets/images/star.png";
 import SpringImage from "@/assets/images/spring.png";
-import Image from "next/image";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -13,25 +27,31 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your fitness progress and motivate your efforts.
           </p>
-          <Image
-            src={StarImage}
+          <motion.img
+            src={StarImage.src}
             alt="Star Image"
             width={360}
             className="absolute -left-[350px]  -top-[137px]"
+            style={{ translateY }}
           />
-          <Image
-            src={SpringImage}
+          <motion.img
+            src={SpringImage.src}
             alt="Spring Image"
             width={360}
             className="absolute -right-[331px] -top-[19px]"
+            style={{ translateY }}
           />
         </div>
         <div className="flex gap-2 mt-10 justify-center">
-          <button className="btn btn-primary">Get for free</button>
-          <button className="btn btn-text gap-1 border border-[#222]/10">
-            <span>Learn more</span>
-            <ArrowRight className="h-5 w-5 fill-black" />
-          </button>
+          <a href="#">
+            <button className="btn btn-primary">Get for free</button>
+          </a>
+          <a href="#">
+            <button className="btn btn-text gap-1 border border-[#222]/10">
+              <span>Learn more</span>
+              <ArrowRight className="h-5 w-5 fill-black" />
+            </button>
+          </a>
         </div>
       </div>
     </section>
