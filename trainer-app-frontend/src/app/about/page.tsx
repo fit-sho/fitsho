9a942cdc3da4838bfc4e92c8252171d66a75cc9a
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-23 01:45:49 -0500

    added usage section

--- trainer-app-frontend/src/app/about/page.tsx
+++ trainer-app-frontend/src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import { Intro } from "@/aboutSections/Intro";
 import { Features } from "@/aboutSections/Features";
+import { Usage } from "@/aboutSections/Usage";
 
 export default function AboutPage() {
   return (
     <>
       <Intro />
       <Features />
+      <Usage />
     </>
   );
 }
