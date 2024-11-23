9342433e027a0f628efe57641a41a9baeda80f03
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-22 20:14:01 -0500

    added features

--- trainer-app-frontend/src/app/about/page.tsx
+++ trainer-app-frontend/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import { Intro } from "@/aboutSections/Intro";
+import { Features } from "@/aboutSections/Features";
 
 export default function AboutPage() {
   return (
     <>
       <Intro />
+      <Features />
     </>
   );
 }
