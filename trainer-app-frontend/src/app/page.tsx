c705429409ab2b3b547e4b26cb53d8dabd4c71e0
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-22 19:26:31 -0500

    removed header and footer since already in layout.tsx

--- trainer-app-frontend/src/app/page.tsx
+++ trainer-app-frontend/src/app/page.tsx
@@ -1,21 +1,17 @@
-import { Header } from "@/homeSections/Header";
 import { Hero } from "@/homeSections/Hero";
 import { TextTicker } from "@/homeSections/TextTicker";
 import { ProductShowcase } from "@/homeSections/ProductShowcase";
 import { Pricing } from "@/homeSections/Pricing";
 import { CallToAction } from "@/homeSections/CallToAction";
-import { Footer } from "@/homeSections/Footer";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <Header />
       <Hero />
       <TextTicker />
       <ProductShowcase />
       <Pricing />
       <CallToAction />
-      <Footer />
     </>
   );
 }
