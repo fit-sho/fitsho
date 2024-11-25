f324008e7651d52a33fe2b3bf18147ad0b3dadb9
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-25 16:13:13 -0500

    initial commit

---
+++ trainer-app-frontend/src/aboutSections/Usage.tsx
@@ -0,0 +1,11 @@
+import { GenericSection } from "./components/GenericSection";
+
+export const Usage = () => {
+  return (
+    <GenericSection
+      title="How to use Fitsho"
+      description=""
+      bgGradient="bg-[radial-gradient(ellipse_250%_100%_at_bottom_left,#183EC2,#EAEEFE_45%)]"
+    />
+  );
+};
