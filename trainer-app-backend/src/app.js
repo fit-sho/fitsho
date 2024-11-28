a16424c9f03ca89cb3dea91ba34aa38059954d0b
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-28 05:38:16 -0500

    setup user model and router plus auth with jwt

--- trainer-app-backend/src/app.js
+++ trainer-app-backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 require("./db/mongoose");
+const userRouter = require("./routers/user");
 
 const app = express();
+
+app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
---
+++ trainer-app-backend/src/models/user.js
@@ -0,0 +1,103 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  // const user = this, "I Could have done this but I wont"
+
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+
+  next(); // We have to call this at the end of the function or else it will always stay on
+});
+
+// accepts 2 arguments, 1st -> string name 2nd -> defenition
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
