6060b5a536393bc9cd6c18bce94c56a9ef0d7437
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-11-28 05:05:10 -0500

    updated generic section to reverse text and images

--- trainer-app-frontend/src/aboutSections/components/GenericSection.tsx
+++ trainer-app-frontend/src/aboutSections/components/GenericSection.tsx
@@ -17,35 +17,36 @@ export const GenericSection = ({
   buttonLink,
   image,
   bgGradient,
-}: GenericSectionProps) => {
+  reverse, // for reversing the order of image and text
+}: GenericSectionProps & { reverse?: boolean }) => {
   return (
     <section className={`pt-8 pb-20 ${bgGradient} overflow-x-clip`}>
       <div className="container">
-        <div className="">
-          <div className="md:flex items-center">
-            <div className="md:w-[478px]">
-              {/* Render button if text and link are provided */}
-              {buttonText && buttonLink && (
-                <a href={buttonLink} className="hover:text-gray-600">
-                  <button className="btn btn-text gap-1 border border-[#222]/40">
-                    <span>{buttonText}</span>
-                    <ArrowRight className="h-5 w-5 fill-black" />
-                  </button>
-                </a>
-              )}
-
-              {/* Render title */}
-              <SectionTitle title={title} className="mt-6" />
-              {/* Render SectionDesc only if description is a string */}
-              {typeof description === "string" && (
-                <SectionDesc description={description} className="mt-6" />
-              )}
-              {/* Render JSX description if provided */}
-              {typeof description !== "string" && (
-                <div className="mt-6">{description}</div>
-              )}
-            </div>
-            <div>{image && image}</div>
+        {/* When the reverse prop is used this will change the image only for md size*/}
+        <div
+          className={`flex flex-col md:flex-row ${
+            reverse ? "md:flex-row-reverse " : ""
+          }`}
+        >
+          {/* When the reverse prop is used this will change the image up and down wise for phone*/}
+          <div className="w-full md:w-1/2 px-6 order-2 md:order-none">
+            {image && image}
+          </div>
+          <div className="w-full md:w-1/2 px-6 order-1 md:order-none">
+            {buttonText && buttonLink && (
+              <a href={buttonLink} className="hover:text-gray-600">
+                <button className="btn btn-text gap-1 border border-[#222]/40">
+                  <span>{buttonText}</span>
+                  <ArrowRight className="h-5 w-5 fill-black" />
+                </button>
+              </a>
+            )}
+            <SectionTitle title={title} className="mt-6" />
+            {typeof description === "string" ? (
+              <SectionDesc description={description} className="mt-6" />
+            ) : (
+              <div className="mt-6">{description}</div>
+            )}
           </div>
         </div>
       </div>
