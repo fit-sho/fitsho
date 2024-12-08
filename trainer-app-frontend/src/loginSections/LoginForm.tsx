996b7a01404dc9dfd477ca61a644824863943733
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-12-08 02:56:15 -0500

    added login user integration

--- trainer-app-frontend/src/loginSections/LoginForm.tsx
+++ trainer-app-frontend/src/loginSections/LoginForm.tsx
@@ -1,7 +1,30 @@
 "use client";
 import { useState } from "react";
+import { loginUser } from "@/utils/userApi";
+import { useRouter } from "next/navigation";
 
 export const LoginForm = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      const { token } = await loginUser(email, password);
+
+      console.log("Token received:", token); // Debugging line
+
+      localStorage.setItem("token", token);
+
+      router.push("/");
+    } catch (error: any) {
+      setError(error.message || "An error occurred. Please try again later.");
+    }
+  };
+
   return (
     <section>
       <div className="min-h-[80vh] flex justify-center items-center pb-16 bg-[radial-gradient(ellipse_250%_100%_at_bottom_right,#183EC2,#EAEEFE_35%)]">
@@ -23,7 +46,7 @@ export const LoginForm = () => {
             </button>
           </div>
 
-          <form className="">
+          <form className="" onSubmit={handleSubmit}>
             {/* Email Input */}
             <div className="space-y-3">
               <div className="block text-base font-medium text-gray-700 mt-3">
@@ -32,6 +55,8 @@ export const LoginForm = () => {
               <input
                 type="email"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="px-3 py-2 border border-gray-300 rounded-lg md:w-[80%] focus:outline-none focus:ring focus:border-blue-300"
                 placeholder="you@example.com"
                 required
@@ -45,11 +70,15 @@ export const LoginForm = () => {
               <input
                 type="password"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="px-3 py-2 border border-gray-300 rounded-lg md:w-[80%] focus:outline-none focus:ring focus:border-blue-300"
                 placeholder="********"
                 required
               />
             </div>
+            {/* Error Message */}
+            {error && <div className="text-red-500 text-sm mt-4">{error}</div>}
             {/* Submit Button */}
 
             <button
