a7bc9732a2fea7e1d2732bf806cd88ad6d5f89ca
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-06-08 02:57:05 -0400

    removed old dashboard

--- src/dashboardSections/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Dashboard } from "./Dashboard";
-export { default as MainSidebar } from "./MainSidebar";
-export { default as SubSidebar } from "./SubSidebar";
-export { default as Header } from "./Header";
